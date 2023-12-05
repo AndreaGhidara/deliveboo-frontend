4ce30039021389b1f2fc678c69b39218d8af4a91
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-12-05 20:15:35 +0100

    type resatuarant and image implmented

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -23,6 +23,9 @@ export default {
                     // handle error
                     console.log(error);
                 });
+        },
+        getTypes(restaurant) {
+            return restaurant.types.map(type => type.name).join(', ');
         }
     },
     created() {
@@ -35,13 +38,22 @@ export default {
 <template>
     <div class="home">
         <h1>Home</h1>
-        <p class="" v-for="restaurant in restaurants">
-            {{ restaurant.name }}
-        </p>
+        <div class="row justify-content-between">
+            <div class="card col-4 mb-4" v-for="restaurant in restaurants" :key="restaurant.id">
+                <img class="img-top img-restaurant p-0" :src="restaurant.photo" alt="">
+                <p>Nome Ristorante: {{ restaurant.name }}</p>
+                <p>Tipologia di Cucina: {{ getTypes(restaurant) }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+
+.img-restaurant{
+    height: 300px;
+    width: 300px;
+}
 .home {
     background-color: skyblue;
 }
